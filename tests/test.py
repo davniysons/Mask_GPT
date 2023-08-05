9d60fc5f34c420c5f65c79bf49cc771161552277
davniysons <tony.ervalson@gmail.com>
2023-08-05 14:06:48 -0230

    try fix

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,11 @@
 import unittest
+import os
+import sys
+
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, parent_dir)
+
+
 from Mask.mask import Mask
 
 class MyTest(unittest.TestCase):
